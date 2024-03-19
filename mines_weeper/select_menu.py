5b1ccae6143ee0796ccdc2b0bca7048c6a77d66f
theo-baccam <theo.baccam@laplateforme.io>
2024-03-19 11:03:00 +0100

    feat: classe Window et SelectMenu

--- mines_weeper/select_menu.py
+++ mines_weeper/select_menu.py
@@ -1,76 +1,64 @@
 import pygame
 import sys
 
-import paths
+from . import paths
+from .window import Window
 
-pygame.init()
 
 # Couleurs
-DARK = (0, 0, 0)
-BRUN = (200, 173, 127)
+class SelectMenu(Window):
+    def __init__(self, WIDTH, HEIGHT):
+        super().__init__(WIDTH, HEIGHT)
 
-# Taille de la fenêtre
-WIDTH, HEIGHT = 350, 350
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Menu de Sélection de Difficulté")
+        self.DARK = (0, 0, 0)
+        self.BRUN = (200, 173, 127)
 
-# Police de texte
-font = pygame.font.Font(None, 36)
+        self.difficulty = self.main()
+        print("Difficulté sélectionnée :", self.difficulty)
 
-def draw_text(text, font, color, surface, x, y):
-    text_obj = font.render(text, True, color)
-    text_rect = text_obj.get_rect()
-    text_rect.center = (x, y)
-    surface.blit(text_obj, text_rect)
+    def main(self):
+        # Chargement de l'image de fond
+        image = pygame.image.load(paths.select_sprite("menu_background.png"))
+        scale = image.get_size()
+        background_img = pygame.transform.scale(image , (350,350))
 
-def main():
+        # Chargement des images des boutons
+        image = pygame.image.load(paths.select_sprite("button.png"))
+        scale = image.get_size()
+        button_img = pygame.transform.scale(image , (int(scale [0] * 2), int(scale [1]* 2)))
 
-    # Chargement de l'image de fond
-    image = pygame.image.load(paths.select_sprite("menu_background.png"))
-    scale = image.get_size()
-    background_img = pygame.transform.scale(image , (350,350))
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # Récupérer les coordonnées de la souris au moment du clic
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    # Vérifier si le clic est sur un des boutons
+                    if easy_button_rect.collidepoint(mouse_x, mouse_y):
+                        return 1
+                    elif normal_button_rect.collidepoint(mouse_x, mouse_y):
+                        return 2
+                    elif expert_button_rect.collidepoint(mouse_x, mouse_y):
+                        return 3
 
-    # Chargement des images des boutons
-    image = pygame.image.load(paths.select_sprite("button.png"))
-    scale = image.get_size()
-    button_img = pygame.transform.scale(image , (int(scale [0] * 2), int(scale [1]* 2)))
+            # Dessiner l'arrière-plan
+            self.screen.fill(self.BRUN)
+            self.screen.blit(background_img, (0, 0))
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                # Récupérer les coordonnées de la souris au moment du clic
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                # Vérifier si le clic est sur un des boutons
-                if easy_button_rect.collidepoint(mouse_x, mouse_y):
-                    return 1
-                elif normal_button_rect.collidepoint(mouse_x, mouse_y):
-                    return 2
-                elif expert_button_rect.collidepoint(mouse_x, mouse_y):
-                    return 3
+            # Dessiner les boutons
+            easy_button_rect = button_img.get_rect(center=(self.WIDTH//2, self.HEIGHT//2 - 50))
+            self.screen.blit(button_img, easy_button_rect)
+            self.draw_text("Facile", self.font, self.DARK, self.screen, self.WIDTH//2, self.HEIGHT//2 - 50)
 
-        # Dessiner l'arrière-plan
-        screen.fill(BRUN)
-        screen.blit(background_img, (0, 0))
+            normal_button_rect = button_img.get_rect(center=(self.WIDTH//2, self.HEIGHT//2))
+            self.screen.blit(button_img, normal_button_rect)
+            self.draw_text("Normal", self.font, self.DARK, self.screen, self.WIDTH//2, self.HEIGHT//2)
 
-        # Dessiner les boutons
-        easy_button_rect = button_img.get_rect(center=(WIDTH//2, HEIGHT//2 - 50))
-        screen.blit(button_img, easy_button_rect)
-        draw_text("Facile", font, DARK, screen, WIDTH//2, HEIGHT//2 - 50)
+            expert_button_rect = button_img.get_rect(center=(self.WIDTH//2, self.HEIGHT//2 + 50))
+            self.screen.blit(button_img, expert_button_rect)
+            self.draw_text("Expert", self.font, self.DARK, self.screen, self.WIDTH//2, self.HEIGHT//2 + 50)
 
-        normal_button_rect = button_img.get_rect(center=(WIDTH//2, HEIGHT//2))
-        screen.blit(button_img, normal_button_rect)
-        draw_text("Normal", font, DARK, screen, WIDTH//2, HEIGHT//2)
-
-        expert_button_rect = button_img.get_rect(center=(WIDTH//2, HEIGHT//2 + 50))
-        screen.blit(button_img, expert_button_rect)
-        draw_text("Expert", font, DARK, screen, WIDTH//2, HEIGHT//2 + 50)
-
-        # Mettre à jour l'affichage
-        pygame.display.flip()
-
-if __name__ == "__main__":
-    difficulty = main()
-    print("Difficulté sélectionnée :", difficulty)
+            # Mettre à jour l'affichage
+            pygame.display.flip()
---
+++ mines_weeper/window.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Window:
+    def __init__(self, WIDTH, HEIGHT):
+        pygame.init()
+
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Menu de Sélection de Difficulté")
+
+        self.font = pygame.font.Font(None, 36)
+
+    def draw_text(self, text, font, color, surface, x, y):
+        text_obj = font.render(text, True, color)
+        text_rect = text_obj.get_rect()
+        text_rect.center = (x, y)
+        surface.blit(text_obj, text_rect)
+
+

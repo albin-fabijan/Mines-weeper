0b61ebb83ea02332052ed8b148d4fddf2245bfb9
theo-baccam <theo.baccam@laplateforme.io>
2024-03-19 11:40:46 +0100

    style(select_menu): supprimé commentaires

--- mines_weeper/select_menu.py
+++ mines_weeper/select_menu.py
@@ -5,7 +5,6 @@ from . import paths
 from .window import Window
 
 
-# Couleurs
 class SelectMenu(Window):
     def __init__(self):
         super().__init__(350, 350)
@@ -24,7 +23,6 @@ class SelectMenu(Window):
         scale = image.get_size()
         background_img = pygame.transform.scale(image , (350,350))
 
-        # Chargement des images des boutons
         image = pygame.image.load(paths.select_sprite("button.png"))
         scale = image.get_size()
         button_img = pygame.transform.scale(image , (int(scale [0] * 2), int(scale [1]* 2)))
@@ -46,9 +44,7 @@ class SelectMenu(Window):
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                # Récupérer les coordonnées de la souris au moment du clic
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                # Vérifier si le clic est sur un des boutons
                 if easy_button_rect.collidepoint(mouse_x, mouse_y):
                     print("Difficulté sélectionnée : 1")
                     self.running = False
@@ -68,11 +64,9 @@ class SelectMenu(Window):
 
         background_img, button_img = self.load_images()
 
-        # Dessiner l'arrière-plan
         self.screen.fill(BRUN)
         self.screen.blit(background_img, (0, 0))
 
-        # Dessiner les boutons
         self.screen.blit(button_img, easy_button_rect)
         self.draw_text("Facile", self.font, DARK, self.screen, self.WIDTH//2, self.HEIGHT//2 - 50)
 
@@ -82,5 +76,4 @@ class SelectMenu(Window):
         self.screen.blit(button_img, expert_button_rect)
         self.draw_text("Expert", self.font, DARK, self.screen, self.WIDTH//2, self.HEIGHT//2 + 50)
 
-        # Mettre à jour l'affichage
         pygame.display.flip()

9f63ccc6eed167232772a76cbe1a59b058d8dc0b
albin-fabijan <163516249+albin-fabijan@users.noreply.github.com>
2024-03-19 12:01:15 +0100

    Update scores.py

--- mines_weeper/scores.py
+++ mines_weeper/scores.py
@@ -5,8 +5,7 @@ import sys
 pygame.init()
 
 # Taille de la fenêtre
-
-SCREEN_WIDTH ,SCREEN_HEIGHT = 800 , 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Affichage des Scores")
 
@@ -19,7 +18,7 @@ BEIGE = (63, 34, 4)
 # Police de texte
 font = pygame.font.SysFont(None, 40)
 
-def afficher_score(timer, niveau, result):
+def afficher_score(timer, niveau, result, pseudo ):
     # Chargement de l'image de fond
     screen.fill(BRUN)
     background_img = pygame.image.load('C:\\Users\\étude\\Documents\\Mines-weeper\\mines_weeper\\1.png')
@@ -54,14 +53,44 @@ def afficher_score(timer, niveau, result):
     quit_button_img.blit(quit_text, quit_rect)
     screen.blit(quit_button_img, (50, SCREEN_HEIGHT - 150))
 
+    # Affichage du pseudo
+    if pseudo == False:
+        text_pseudo = font.render("Entrée votre pseudo." , True , BLACK)
+        pseudo_rect = text_pseudo.get_rect(center=(button_img.get_width(), button_img.get_height()))
+        button_img.blit(text_pseudo,pseudo_rect)
+    else:
+        text_pseudo = font.render(f"Pseudo: {pseudo}", True, BLACK)
+        pseudo_rect = text_pseudo.get_rect(center=(button_img.get_width() // 2, button_img.get_height() // 4))
+        button_img.blit(text_pseudo, pseudo_rect)
+
+
     pygame.display.flip()
+
+def saisir_pseudo():
+    pseudo = ""
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return pseudo
+                elif event.key == pygame.K_BACKSPACE:
+                    pseudo = pseudo[:-1]
+                else:
+                    pseudo += event.unicode
 
 def main():
     timer = 60  # Exemple de timer
     niveau = 1  # Exemple de niveau de difficulté
-    result = True #exemple de resultat
+    result = True  # Exemple de resultat
+    pseudo = False
+    
 
-    afficher_score(timer, niveau , result)
+    afficher_score(timer, niveau, result , pseudo )
+
+    pseudo = saisir_pseudo()
+
+    afficher_score(timer, niveau, result , pseudo)
+
 
     while True:
         for event in pygame.event.get():

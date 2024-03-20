5c18a144122a410f9997421d70efd5a70c6b59af
theo-baccam <theo.baccam@laplateforme.io>
2024-03-20 15:13:40 +0100

    feat: Dialogue pour entrer nom dans menu principal

--- mines_weeper/select_menu.py
+++ mines_weeper/select_menu.py
@@ -10,22 +10,28 @@ class SelectMenu(Window):
         pygame.display.set_caption("Menu de Sélection de Difficulté")
         self.font = pygame.font.Font(None, 36)
 
+        self.player_name = ""
+        self.PLAYER_NAME_LENGTH = 8
+
         self.main_loop()
 
     def main_loop(self):
         while self.running:
             (
+                player_name_rect,
                 easy_button_rect,
                 normal_button_rect,
                 expert_button_rect
             ) = self.get_rects()
 
             self.input_loop(
+                player_name_rect,
                 easy_button_rect,
                 normal_button_rect,
                 expert_button_rect
             )
             self.display(
+                player_name_rect,
                 easy_button_rect,
                 normal_button_rect,
                 expert_button_rect
@@ -35,43 +41,61 @@ class SelectMenu(Window):
         p = Paths()
         image = pygame.image.load(p.select_sprite("menu_background.png"))
         scale = image.get_size()
-        background_img = pygame.transform.scale(image , (350,350))
+        background_img = pygame.transform.scale(
+            image ,
+            (self.WIDTH, self.HEIGHT)
+        )
 
         image = pygame.image.load(p.select_sprite("button.png"))
         scale = image.get_size()
         button_img = pygame.transform.scale(
             image,
-            (int(scale [0] * 2), int(scale [1]* 2))
+            (int(scale[0] * 2), int(scale[1]* 2))
         )
 
-        return background_img, button_img
+        image = pygame.image.load(p.select_sprite("text_field.png"))
+        scale = image.get_size()
+        player_name_img = pygame.transform.scale(
+            image,
+            (int(scale[0] * 2), int(scale[1] * 2))
+        )
+
+        return background_img, button_img, player_name_img
 
     def get_rects(self):
-        background_img, button_img = self.load_images()
+        background_img, button_img, player_name_img = self.load_images()
+
+        player_name_rect = player_name_img.get_rect(
+            center=(
+                self.WIDTH // 2,
+                self.HEIGHT // 2 - 60
+            )
+        )
 
         easy_button_rect = button_img.get_rect(
             center=(
-                self.WIDTH//2,
-                self.HEIGHT//2 - 50
+                self.WIDTH // 2,
+                self.HEIGHT // 2 
             )
         )
         normal_button_rect = button_img.get_rect(
             center=(
-                self.WIDTH//2,
-                self.HEIGHT//2
+                self.WIDTH // 2,
+                self.HEIGHT // 2 + 50
             )
         )
         expert_button_rect = button_img.get_rect(
             center=(
-                self.WIDTH//2,
-                self.HEIGHT//2 + 50
+                self.WIDTH // 2,
+                self.HEIGHT // 2 + 100
             )
         )
 
-        return easy_button_rect, normal_button_rect, expert_button_rect
+        return player_name_rect, easy_button_rect, normal_button_rect, expert_button_rect
 
     def input_loop(
         self,
+        player_name_rect,
         easy_button_rect,
         normal_button_rect,
         expert_button_rect
@@ -80,6 +104,17 @@ class SelectMenu(Window):
             if event.type == pygame.QUIT:
                 self.running = False
                 break
+
+            if event.type == pygame.KEYDOWN:
+                if pygame.K_a <= event.key <= pygame.K_z:
+                    if len(self.player_name) == self.PLAYER_NAME_LENGTH:
+                        continue
+                    self.player_name = (
+                        self.player_name
+                        + pygame.key.name(event.key).upper()
+                    )
+                if event.key == pygame.K_BACKSPACE:
+                    self.player_name = self.player_name[:-1]
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -100,6 +135,7 @@ class SelectMenu(Window):
 
     def display(
         self,
+        player_name_rect,
         easy_button_rect,
         normal_button_rect,
         expert_button_rect
@@ -107,10 +143,28 @@ class SelectMenu(Window):
         DARK = (0, 0, 0)
         BRUN = (200, 173, 127)
 
-        background_img, button_img = self.load_images()
+        background_img, button_img, player_name_img = self.load_images()
 
         self.screen.fill(BRUN)
         self.screen.blit(background_img, (0, 0))
+
+        self.screen.blit(player_name_img, player_name_rect)
+        self.draw_text(
+            "Entrez votre nom :",
+            self.font,
+            DARK,
+            self.screen,
+            self.WIDTH // 2,
+            self.HEIGHT // 2 - 120
+        )
+        self.draw_text(
+            self.player_name,
+            self.font,
+            DARK,
+            self.screen,
+            self.WIDTH // 2,
+            self.HEIGHT // 2 - 75
+        )
 
         self.screen.blit(button_img, easy_button_rect)
         self.draw_text(
@@ -118,8 +172,8 @@ class SelectMenu(Window):
             self.font,
             DARK,
             self.screen,
-            self.WIDTH//2,
-            self.HEIGHT//2 - 50
+            self.WIDTH // 2,
+            self.HEIGHT // 2
         )
 
         self.screen.blit(button_img, normal_button_rect)
@@ -128,8 +182,8 @@ class SelectMenu(Window):
             self.font,
             DARK,
             self.screen,
-            self.WIDTH//2,
-            self.HEIGHT//2
+            self.WIDTH // 2,
+            self.HEIGHT // 2 + 50
         )
 
         self.screen.blit(button_img, expert_button_rect)
@@ -138,8 +192,8 @@ class SelectMenu(Window):
             self.font,
             DARK,
             self.screen,
-            self.WIDTH//2,
-            self.HEIGHT//2 + 50
+            self.WIDTH // 2,
+            self.HEIGHT // 2 + 100
         )
 
         pygame.display.flip()

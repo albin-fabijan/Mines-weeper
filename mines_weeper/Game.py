afffe577a3bd5eefa8cade2136bd7114e8e93bc5
theo-baccam <theo.baccam@laplateforme.io>
2024-03-21 11:31:22 +0100

    refactor: Game devient une classe

--- mines_weeper/Game.py
+++ mines_weeper/Game.py
@@ -1,58 +1,52 @@
 import pygame 
 
 from paths import Paths
+from window import Window
 import ClickableSprite as ClickableSprite
 import Case as C
 import Matrix as Matrix
+
+class Game(Window):
+    def __init__(self, WIDTH, HEIGHT, bomb_number):
+        self.matrix_size = 20
+
+        super().__init__((matrix_size * 32, matrix_size * 32 + 64))
+
+        self.bomb_number = bomb_number
+
+        self.not_clicked = pygame.image.load(Paths().select_sprite("grid.png")).convert()
+        self.sprite = ClickableSprite.ClickableSprite(not_clicked, 50, 50, on_left_click, on_right_click)
+
+
+        self.group = pygame.sprite.Group(sprite)
+        self.group.remove(sprite)
+
+        self.matrix = Matrix.Matrix((5, 10), matrix_size, group)
+
+        for i in range(self.matrix.get_matrix_size()) :
+            for j in range(self.matrix.get_matrix_size()) :
+                group.add(self.matrix.get_case((i,j)).get_sprite())
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if self.matrix.check_lose() :
+                    print("lose")
+                if self.matrix.check_win() :
+                    print("win")
+
+
+            self.group.update(events)
+            self.screen.fill((255, 255, 255))
+            self.group.draw(screen)
+            pygame.display.update()
+        
+        pygame.quit()
+
  
-def on_left_click():
-    sprite.image = pygame.image.load(Paths().select_sprite("empty.png")).convert()
+    def on_left_click(self):
+        sprite.image = pygame.image.load(Paths().select_sprite("empty.png")).convert()
 
-def on_right_click() :
-    sprite.image = pygame.image.load(Paths().select_sprite("flag.png")).convert()
-
-dif = 0
-ok = False
-while (not ok) :
-    dif = int(input("difficulty (1-3) : "))
-    if (not (dif < 1 or dif > 3)) :
-        ok = True
-    print(ok)
-
-matrix_size = 20
-
-pygame.init()
-screen = pygame.display.set_mode((matrix_size*32, matrix_size*32+64))
-
-not_clicked = pygame.image.load(Paths().select_sprite("grid.png")).convert()
-
-sprite = ClickableSprite.ClickableSprite(not_clicked, 50, 50, on_left_click, on_right_click)
-
-group = pygame.sprite.Group(sprite)
-group.remove(sprite)
-
-
-matrix = Matrix.Matrix((5, 10), matrix_size, group)
-
-for i in range(matrix.get_matrix_size()) :
-    for j in range(matrix.get_matrix_size()) :
-        group.add(matrix.get_case((i,j)).get_sprite())
-
-running = True
-while running:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            running = False
-        if matrix.check_lose() :
-            print("lose")
-        if matrix.check_win() :
-            print("win")
-
-
-    group.update(events)
-    screen.fill((255, 255, 255))
-    group.draw(screen)
-    pygame.display.update()
- 
-pygame.quit()
+    def on_right_click(self):
+        sprite.image = pygame.image.load(Paths().select_sprite("flag.png")).convert()

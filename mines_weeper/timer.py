325090654c5f85babd4e397ecfe97947a0eac76e
albin-fabijan <163516249+albin-fabijan@users.noreply.github.com>
2024-03-21 08:42:58 +0100

    add timer

---
+++ mines_weeper/timer.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+# Couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BRUN = (200, 173, 127)
+BEIGE = (63, 34, 4)
+
+# Taille de la fenêtre
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Scores")
+
+def main ():
+
+
+def timer():
+
+def nb_bombe ():
+
+def affichage():
+
+
+#init

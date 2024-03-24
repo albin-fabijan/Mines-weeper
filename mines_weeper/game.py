ae85c4ff584b40f0a1ea0db32b9687289cedf708
theo-baccam <theo.baccam@laplateforme.io>
2024-03-24 17:14:13 +0100

    style(Game.py): PEP-8

--- mines_weeper/game.py
+++ mines_weeper/game.py
@@ -15,10 +15,17 @@ class Game(Window):
         self.start_time = 0
         self.game_started = False
 
-        self.background_image = pygame.image.load(Paths().select_sprite("background_ingame.png"))
-        self.background_image = pygame.transform.scale(self.background_image, (matrix_size * 32 , 64))
+        self.background_image = pygame.image.load(
+                Paths().select_sprite("background_ingame.png")
+        )
+        self.background_image = pygame.transform.scale(
+                self.background_image,
+                (matrix_size * 32 , 64)
+        )
 
-        self.not_clicked = pygame.image.load(Paths().select_sprite("Grid.png")).convert()
+        self.not_clicked = pygame.image.load(
+                Paths().select_sprite("Grid.png")
+        ).convert()
 
         self.sprite = self.sprite_initialization()
         self.group = self.group_initialization()
@@ -48,6 +55,7 @@ class Game(Window):
                     pygame.time.wait(3000)
                     self.running = False
                     self.win = False
+
                 if self.matrix.check_win():
                     self.final_time = elapsed_time
                     pygame.time.wait(3000)
@@ -59,13 +67,16 @@ class Game(Window):
                 self.game_started = True
                 
             self.group.update(events)
+
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background_image, (0, 0))
-            self.group.draw(self.screen)
+            self.group.draw(self.screen)*
+
             elapsed_time = self.display_timer()
             self.display_numbombs()
             self.display_numflags()
             self.display_numinterro()
+
             button_img, restart_button_rect = self.get_restart_button()
             self.screen.blit(button_img, restart_button_rect)
             self.draw_text(
@@ -76,14 +87,15 @@ class Game(Window):
                 self.WIDTH - 24,
                 48
             )
+
             pygame.display.flip()
         
         pygame.quit()
 
         score = ScoreScreen(800, 600)
-        while (not self.restart) :
+        while (not self.restart):
             restart = score.run(self.final_time, self.difficulty ,self.win)
-            if (restart == True) :
+            if (restart == True):
                 self.restart = True
     
     def display_timer(self):
@@ -96,43 +108,59 @@ class Game(Window):
         seconds = int(elapsed_time % 60)
 
         font = pygame.font.SysFont(None, 30)
-        timer_text = font.render(f"Time: {minutes:02}:{seconds:02}", True, (0, 0, 0))
+        timer_text = font.render(
+                f"Time: {minutes:02}:{seconds:02}",
+                True,
+                (0, 0, 0)
+        )
         self.screen.blit(timer_text, (5, 10))
         return elapsed_time
     
     def display_numbombs(self):
         font = pygame.font.SysFont(None, 30)
-        timer_text = font.render(f"Bombs: {self.num_bombs()- self.num_flags()}", True, (0, 0, 0))
+        timer_text = font.render(
+                f"Bombs: {self.num_bombs()- self.num_flags()}",
+                True,
+                (0, 0, 0)
+        )
         self.screen.blit(timer_text, (5, 30))
         return
 
     def display_numflags(self):
         font = pygame.font.SysFont(None, 30)
-        timer_text = font.render(f"Flags: {self.num_flags()}", True, (0, 0, 0))
+        timer_text = font.render(
+                f"Flags: {self.num_flags()}",
+                True,
+                (0, 0, 0)
+        )
         self.screen.blit(timer_text, (self.matrix_size * 15, 10))
         return
     
     def display_numinterro(self):
         font = pygame.font.SysFont(None, 30)
-        timer_text = font.render(f"?: {self.num_interrogations()}", True, (0, 0, 0))
+        timer_text = font.render(
+                f"?: {self.num_interrogations()}",
+                True,
+                (0, 0, 0)
+        )
         self.screen.blit(timer_text, (self.matrix_size * 15, 30))
         return
 
-    def num_bombs(self) :
+    def num_bombs(self):
         bombs = 0
         for c in self.matrix.get_cases() :
             if (c.bomb) :
                 bombs += 1
         return bombs
     
-    def num_flags(self) :
+    def num_flags(self):
         flags = 0
         for c in self.matrix.get_cases() :
             if (c.flag == 1) :
                 flags += 1
         return flags
     
-    def num_interrogations(self) :
+    def num_interrogations(self):
         interro = 0
         for c in self.matrix.get_cases() :
             if (c.flag == 2) :
@@ -140,10 +168,14 @@ class Game(Window):
         return interro
 
     def on_left_click(self):
-        sprite.image = pygame.image.load(Paths().select_sprite("empty.png")).convert()
+        sprite.image = pygame.image.load(
+                Paths().select_sprite("empty.png")
+        ).convert()
 
     def on_right_click(self):
-        sprite.image = pygame.image.load(Paths().select_sprite("flag.png")).convert()
+        sprite.image = pygame.image.load(
+                Paths().select_sprite("flag.png")
+        ).convert()
 
     def get_restart_button(self):
         image = pygame.image.load(Paths().select_sprite("select.png"))
